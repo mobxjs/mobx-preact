a59c7fbbd4026516cfc818962df78a1f44cae435
Ryan Megidov <ryan@megidov.com>
2016-12-27 16:48:47 +0100

    Fix exports

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import Provider from './Provider';
 import { renderReporter, componentByNodeRegistery } from './makeReactive';
 import connect from './connect';
 
+export {
+	Provider,
+	connect,
+	connect as observer,
+	renderReporter,
+	componentByNodeRegistery
+}
 export default {
 	Provider,
 	connect,
