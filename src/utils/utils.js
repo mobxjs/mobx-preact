6f5ccf3385ae0fea021e3e54ddc9f89908c945a5
Phil Mander <philip.mander@gmail.com>
2017-12-03 01:52:45 +0100

    Allow `Provider` to be passed multiple children
    If this happens then they will be wrapped with a `div` element
    automatically, instead of failing.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -6,15 +6,6 @@ export function isStateless(component) {
     return !(component.prototype && component.prototype.render) && !Component.isPrototypeOf(component);
 }
 
-// adapted from https://github.com/developit/preact-compat/blob/3.17.0/src/index.js#L204
-export function childrenOnly(children) {
-    children = children == null ? [] : [].concat(children);
-    if (children.length !== 1) {
-        throw new Error('Children.only() expects only one child.');
-    }
-    return children[0];
-}
-
 export function makeDisplayName(component, { prefix = '', suffix = ''} = {}) {
     let displayName =
         (component.displayName || component.name || (component.constructor && component.constructor.name) || '<component>');
